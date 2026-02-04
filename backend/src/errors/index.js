d9b25fedb2a9955a43989c609e9587045bd93dfd
ub-victor <victoireushindi371@gmail.com>
2026-02-04 15:49:54 +0200

    Initial commit

---
+++ backend/src/errors/index.js
@@ -0,0 +1,19 @@
+const {
+  CustomAPIError,
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+  UnauthorizedError,
+  DuplicateKeyError,
+  ValidationError
+} = require('./customErrors');
+
+module.exports = {
+  CustomAPIError,
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+  UnauthorizedError,
+  DuplicateKeyError,
+  ValidationError
+};
