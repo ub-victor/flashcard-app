4f5350bfecf12d4aee47383fa13b4ad4567e6133
ub-victor <victoireushindi371@gmail.com>
2026-02-06 00:52:32 +0200

    config folder creation plus the db file

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require ('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    } catch(error){
+        console.error(`Error: ${error.message}`)
+    }
+}
