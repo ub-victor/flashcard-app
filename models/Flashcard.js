a3c8535a8789a1608c8106e85f7a5025782d0b1e
ub-victor <victoireushindi371@gmail.com>
2026-02-11 14:19:47 +0200

    Make the modal of my app

--- models/Flashcard.js
+++ models/Flashcard.js
@@ -51,7 +51,7 @@ const flashcardSchema = new mongoose.Schema({
         type: String,
         default: 'User'
     }
-}, { // Schema Option second Arguments
+}, { // Schema Option second Arguments with purpose of Automatic date,
     timestamps: true, //Mongoose automatically adds two fields to this document: createdAt, updatedAt
     toJSON: { virtuals: true }, // include virtual fields when converting to JSON.
     toObject: { virtuals: true }
