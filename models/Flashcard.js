a76491ed142727d958555b503ad012398aa12453
ub-victor <victoireushindi371@gmail.com>
2026-02-11 11:17:35 +0200

    Make the modal of my app

--- models/Flashcard.js
+++ models/Flashcard.js
@@ -7,6 +7,7 @@ const flashcardSchema = new mongoose.Schema({
         require: [true, 'Please provide a question'],
         trim: true,
         minlength: [3, 'Question must be at least 3 characters'],
+        maxlength: [500, 'Question cannot exceed 500 chararacters']
 
     }
 })
