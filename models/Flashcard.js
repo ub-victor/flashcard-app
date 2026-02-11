1e6139d138093815cceb2e4a83cfb8b1c997c7da
ub-victor <victoireushindi371@gmail.com>
2026-02-11 11:15:02 +0200

    Make the modal of my app

--- models/Flashcard.js
+++ models/Flashcard.js
@@ -4,6 +4,7 @@ const { type } = require('os');
 const flashcardSchema = new mongoose.Schema({
     question: {
         type: String,
-        require: [true, 'Please provide a question']
+        require: [true, 'Please provide a question'],
+        trim: true
     }
 })
