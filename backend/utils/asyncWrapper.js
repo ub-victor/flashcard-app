0d9d09b8b263a99b037885a6d4e6505768e59068
ub-victor <victoireushindi371@gmail.com>
2026-02-11 10:11:52 +0200

    Async Wrapper

--- backend/utils/asyncWrapper.js
+++ backend/utils/asyncWrapper.js
@@ -1,6 +1,7 @@
 // Async wrapper to eliminate try/catch in controllers
 
 const asyncWrapper = (fn)=> {
+    // it returns another function because Express expects route handlers in this format
     return async (req, res, next)=>{
         try{
             await fn(req, res, next);
