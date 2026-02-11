0da073e4c1a296fe8d5aa87ff1602d76dae28f8b
ub-victor <victoireushindi371@gmail.com>
2026-02-11 19:33:01 +0200

    add indexes

--- controllers/flashcards.js
+++ controllers/flashcards.js
@@ -1,3 +1,2 @@
 const Flashcard = require('../models/Flashcard');
 const asyncWrapper = require('../utils/asyncWrapper');
-const {BadRequestError, NotFoundError, ValidationError, DuplicateKeyError} = require('../erros');
