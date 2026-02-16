aaa51f20a8685188c7c2f3afd97e3a946f38834b
ub-victor <victoireushindi371@gmail.com>
2026-02-16 13:41:15 +0200

    making the controller

--- backend/controllers/flashcards.js
+++ backend/controllers/flashcards.js
@@ -1,3 +1,3 @@
 const Flashcard = require('../models/Flashcard');
 const asyncWrapper = require('../utils/asyncWrapper');
-const {BadResquestError, } = require('../')
+const {BadResquestError, NotFoundError} = require('../')
