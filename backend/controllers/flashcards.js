6e3734a943657cec39fee739c43ef06618d2f5f6
ub-victor <victoireushindi371@gmail.com>
2026-02-16 13:55:11 +0200

    making the controller

--- backend/controllers/flashcards.js
+++ backend/controllers/flashcards.js
@@ -10,5 +10,5 @@ const {BadResquestError, NotFoundError, ValidationError, DuplicateKeyError} = re
 const getAllFlashcards = asyncWrapper(async(req, res)=>{
     const page = parseInt(req.query.page) || 1; 
     const limit = parseInt(req.query.limit) || 10;
-    const skip = (page -1)
+    const skip = (page -1) * limit;
 });
